08a55fb1de0d1d77b59210e7d6dac38337a221d8
jairosang <jairosanmiguelsang@gmail.com>
2025-03-11 12:52:01 +0100

    added working keyboard input and player movement system

--- main.asm
+++ main.asm
@@ -1,12 +1,33 @@
-.data 
+# Imported functions:
+# INIT_UTILITIES_ADDRS, INIT_DISPLAY, GET_KEYBOARD, dspl_check_and_print
+# update_p_up, update_p_left, update_p_down, update_p_right
 .text
+jal INIT_UTILITIES_ADDRS
+
 main:
-	jal DISPLAY
 	
-	jal increase_score
+	jal INIT_DISPLAY
 	
-	jal DISPLAY
+input_loop:
+	jal GET_KEYBOARD
+	beq $v0, 119, move_p_up
+	beq $v0, 97, move_p_left
+	beq $v0, 115, move_p_down
+	beq $v0, 100, move_p_right
 	
+move_p_up:
+	jal update_p_up
+	j main
 	
-	li $v0, 10
-	syscall
+move_p_left:
+	jal update_p_left
+	j main
+
+move_p_down:
+	jal update_p_down
+	j main
+
+move_p_right:
+	jal update_p_right
+	j main
+	
--- utilities.asm
+++ utilities.asm
@@ -1,23 +1,35 @@
 # https://wilkinsonj.people.charleston.edu/mmio.html
 # https://www.ascii-code.com
-.globl main
 
+# RESERVED $s0, $s1 FOR DISPLAY
+# RESERVED $s2, $s3 FOR KEYBOARD
+# RESERVED $s7 FOR ARRAY DISPLAY
 .data
 	DISPLAY_CTRL_R: .word 0xFFFF0008 #Display control register
 	DISPLAY_ADDR: .word 0xFFFF000C  # Display address
 	KEYBOARD_STATUS: .word 0xFFFF0000  # Address to check IF a key is pressed
 	KEYBOARD_DATA: .word 0xFFFF0004    # Address to read THE key pressed
 	
-	score_label: .asciiz "    Score:"
+	scoreLabel: .asciiz "    Score:"
 	score: .byte '0'
 	border: .byte '#'
 	space: .byte ' '
 	player: .byte 'P'
 	reward: .byte 'R'
-	board: .asciiz "                                                 "
+	board: .asciiz "P                                                "
+	playerLocation: .byte 0
 	
 .text 
-main:
+.globl INIT_UTILITIES_ADDRS, INIT_DISPLAY, GET_KEYBOARD, dspl_check_and_print, update_p_up, update_p_left, update_p_down, update_p_right
+INIT_UTILITIES_ADDRS:
+
+	lw $s0, DISPLAY_CTRL_R       # Copy the control register into a CPU register.
+	lw $s1, DISPLAY_ADDR	# Load the address of the display into $t0
+	lw $s2, KEYBOARD_STATUS
+	lw $s3, KEYBOARD_DATA
+	
+	jr $ra
+	
 # DATA FUNCTIONS
 #=============================================================================================================================================================================================
 increase_score:
@@ -26,8 +38,106 @@ increase_score:
 	sb $t0, score
 
 	jr $ra
+	
+	
+# CHARACTER FUNCTIONS
+#=============================================================================================================================================================================================
+update_p_up:
+	# Load player location and board address
+	lb $t0, playerLocation
+	la $t1, board
+	
+	# Move to board's player location address and make it blank
+	add $t1, $t1, $t0
+	li $t2, 32
+	sb $t2, ($t1)
+	
+	# Store the new player location
+	addi $t0, $t0, -7
+	sb $t0, playerLocation
+	blt $t0, 0, game_over
+	bgt $t0, 49, game_over
+	
+	# Move to new player location and make it 'P'
+	addi $t1, $t1, -7
+	lb $t2, player
+	sb $t2, ($t1)
+	
+	jr $ra
 
+update_p_left:
+	# Load player location and board address
+	lb $t0, playerLocation
+	la $t1, board
+	
+	# Move to board's player location address and make it blank
+	add $t1, $t1, $t0
+	li $t2, 32
+	sb $t2, ($t1)
+	
+	# Store the new player location
+	addi $t0, $t0, -1
+	sb $t0, playerLocation
+	blt $t0, 0, game_over
+	bgt $t0, 49, game_over
+	
+	# Move to new player location and make it 'P'
+	addi $t1, $t1, -1
+	lb $t2, player
+	sb $t2, ($t1)
+	
+	
+	
+	jr $ra
 
+update_p_down:	
+	# Load player location and board address
+	lb $t0, playerLocation
+	la $t1, board
+	
+	# Move to board's player location address and make it blank
+	add $t1, $t1, $t0
+	li $t2, 32
+	sb $t2, ($t1)
+	
+	# Store the new player location
+	addi $t0, $t0, 7
+	sb $t0, playerLocation
+	blt $t0, 0, game_over
+	bgt $t0, 49, game_over
+	
+	# Move to new player location and make it 'P'
+	addi $t1, $t1, 7
+	lb $t2, player
+	sb $t2, ($t1)
+	
+	jr $ra
+	
+update_p_right:
+	# Load player location and board address
+	lb $t0, playerLocation
+	la $t1, board
+	
+	# Move to board's player location address and make it blank
+	add $t1, $t1, $t0
+	li  $t2, 32
+	sb $t2, ($t1)
+	
+	# Store the new player location
+	addi $t0, $t0, 1
+	sb $t0, playerLocation
+	blt $t0, 0, game_over
+	bgt $t0, 49, game_over
+	
+	# Move to new player location and make it 'P'
+	addi $t1, $t1, 1
+	lb $t2, player
+	sb $t2, ($t1)
+		
+	jr $ra
+	
+game_over:
+	jr $ra
 
 # DISPLAY FUNCTIONS
 #=============================================================================================================================================================================================
@@ -83,7 +193,7 @@ display_border:
 
 # Loops to display the word "Score: "
 display_score:
-	la $t1, score_label			# Load the address of string into $t1
+	la $t1, scoreLabel			# Load the address of string into $t1
 	addi $sp, $sp, -4
 	sw  $ra, 4($sp)
 loop_display_score:
@@ -128,7 +238,7 @@ exit_loop_display_top_border:
 	
 # Initializes the board display to display from beginning
 init_display_board:
-	la $s3, board	# Load address of board
+	la $s7, board	# Load address of board
 display_board:
 	li $t9, 1	# Starts a counter to keep track of characters printed
 	addi $sp, $sp, -4
@@ -136,12 +246,12 @@ display_board:
 	
 loop_display_board_line:
 	jal print_space
-	lb $a0, ($s3)		# Stores the according letter value in $a0
+	lb $a0, ($s7)		# Stores the according letter value in $a0
 	seq $t1, $t9, 7		# If the value is null, the word ended 
 	jal dspl_check_and_print
 	
 	# Goes to next character in array
-	addi $s3, $s3, 1
+	addi $s7, $s7, 1
 	addi $t9, $t9, 1
 	
 	bne  $t1, 0, exit_display_board_line	
@@ -156,12 +266,9 @@ exit_display_board_line:
 	
 	
 # MAIN DISPLAY FUNCTION
-DISPLAY:
+INIT_DISPLAY:
 	addi $sp, $sp, -4
 	sw $ra, 4($sp)
-	
-	lw $s0, DISPLAY_CTRL_R       # Copy the control register into a CPU register.
-	lw $s1, DISPLAY_ADDR	# Load the address of the display into $t0
 	
 	li $a0, 12
 	jal dspl_check_and_print
@@ -203,9 +310,17 @@ exit_dspl_loop:
 
 # KEYBOARD FUNCTIONS
 #============================================================================================================================================================================================
+# MAIN KEYBOARD FUNCTION
+GET_KEYBOARD:
+keyboard_check_and_get:
 
-
-
+	lw $t0, 0($s2)
+        andi $t0, $t0, 1        # Mask off (turn to zero) all of the bits in $t0 except the ready bit.
+                                # At this point the value in $t0 is zero if the ready bit was clear or nonzero if it was set.
+        beq $t0, $zero, keyboard_check_and_get
+	
+	lw $v0, 0($s3) # GET CHARACTER
+	jr $ra
 
 
 
